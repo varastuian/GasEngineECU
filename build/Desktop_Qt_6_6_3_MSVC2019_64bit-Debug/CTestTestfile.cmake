58835e9f17e5b7add8310af55b99146db12dea4d
Vahid <varastuian@gmail.com>
2025-02-02 10:39:23 +0330

    Delete build/Desktop_Qt_6_6_3_MSVC2019_64bit-Debug directory

--- build/Desktop_Qt_6_6_3_MSVC2019_64bit-Debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/varas/OneDrive/Documents/Codes/ECU/second/ParadECU
-# Build directory: C:/Users/varas/OneDrive/Documents/Codes/ECU/second/ParadECU/build/Desktop_Qt_6_6_3_MSVC2019_64bit-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
